80603159a8851dc66013447a23df77d24dbe1dec
mark mamdouh <73199677+markdoha@users.noreply.github.com>
2022-06-14 19:16:54 +0200

    fixing

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(
-  "postgres://postgres:020508m.m@database-1.ctwv75pzrl9w.us-east-1.rds.amazonaws.com:5432/postgres"
-);
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
