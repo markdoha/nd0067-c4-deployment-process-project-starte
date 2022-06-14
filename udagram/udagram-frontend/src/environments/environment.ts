979be0f744143febe3197cc7d049db9cd4583841
mark mamdouh <73199677+markdoha@users.noreply.github.com>
2022-06-15 01:08:21 +0200

    fix

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'udagram-api2',
-  apiHost: 'http://udagram-api2-dev222.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api2-dev222.us-east-1.elasticbeanstalk.com',
 };
 
 /*
