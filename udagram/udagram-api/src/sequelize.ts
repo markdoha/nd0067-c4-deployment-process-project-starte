502c4461a2d169fd50ba3a0074e7d477e989ed28
mark mamdouh <73199677+markdoha@users.noreply.github.com>
2022-06-14 18:51:01 +0200

    adding database

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(
+  "postgres://postgres:020508m.m@database-1.ctwv75pzrl9w.us-east-1.rds.amazonaws.com:5432/postgres"
+);
